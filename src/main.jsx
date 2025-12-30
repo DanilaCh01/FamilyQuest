642dfa308fedf35ebde0dd2a0a2b1c8b16f68709
DanilaCh01 <117128628+DanilaCh01@users.noreply.github.com>
2025-12-30 11:33:37 +0200

    the file structure update

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-//import { App } from './app';
 
-import { Routing } from './routing/routing.component';
+
+import { Routing } from './routing';
 
 const root = document.getElementById('root');
 
